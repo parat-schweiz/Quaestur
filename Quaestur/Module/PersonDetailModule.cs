fb96f4cb88a56243b9d7f3768b556dab8edee60c
Stefan Thöni <stefan@savvy.ch>
2019-03-13 02:07:02 +0100

    Updated dependencies

--- Quaestur/Module/PersonDetailModule.cs
+++ Quaestur/Module/PersonDetailModule.cs
@@ -53,28 +53,6 @@ namespace Quaestur
         }
     }
 
-    public class DialogViewModel
-    {
-        public string Title;
-        public string DialogId;
-        public string ButtonId;
-        public string PhraseButtonOk;
-        public string PhraseButtonCancel;
-
-        public DialogViewModel()
-        { 
-        }
-
-        public DialogViewModel(Translator translator, string title, string dialogId)
-        {
-            PhraseButtonOk = translator.Get("Dialog.Button.OK", "Button 'OK' in any dialog", "OK").EscapeHtml();
-            PhraseButtonCancel = translator.Get("Dialog.Button.Cancel", "Button 'Cancel' in any dialog", "Cancel").EscapeHtml();
-            Title = title.EscapeHtml();
-            DialogId = dialogId;
-            ButtonId = dialogId + "Button";
-        }
-    }
-
     public class PersonDetailHeadViewModel
     {
         public string Id;
