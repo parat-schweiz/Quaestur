28db59373fadc4cd74491c6ea562693a9f83aeeb
Stefan Thöni <stefan.thoeni@ppzs.ch>
2019-10-26 17:14:10 +0200

    Added Redmine API

---
+++ RedmineEngagement/EngagementConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using BaseLibrary;
+using QuaesturApi;
+using RedmineApi;
+
+namespace RedmineEngagement
+{
+    public class EngagementConfig : Config
+    {
+        public QuaesturApiConfig QuaesturApi { get; private set; }
+        public RedmineApiConfig RedmineApi { get; private set; }
+        public ConfigSectionDatabase Database { get; private set; }
+        public string LogFilePrefix { get; private set; }
+
+        public EngagementConfig()
+        {
+            QuaesturApi = new QuaesturApiConfig();
+            RedmineApi = new RedmineApiConfig();
+            Database = new ConfigSectionDatabase();
+        }
+
+        public override IEnumerable<ConfigSection> ConfigSections
+        {
+            get
+            {
+                yield return QuaesturApi;
+                yield return RedmineApi;
+                yield return Database;
+            }
+        }
+
+        public override IEnumerable<ConfigItem> ConfigItems
+        {
+            get
+            {
+                yield return new ConfigItemString("LogFilePrefix", v => LogFilePrefix = v);
+            }
+        }
+    }
+}
---
+++ RedmineEngagement/EngagementMaster.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using BaseLibrary;
+using SiteLibrary;
+using QuaesturApi;
+using RedmineApi;
+
+namespace RedmineEngagement
+{
+    public class EngagementMaster
+    {
+        private readonly EngagementConfig _config;
+        private readonly Logger _logger;
+        private readonly Quaestur _quaestur;
+        private readonly Redmine _redmine;
+        private readonly IDatabase _database;
+
+        public EngagementMaster(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException("Config file not found");
+            }
+
+            _config = new EngagementConfig();
+            _config.Load(filename);
+
+            _logger = new Logger(_config.LogFilePrefix);
+            _logger.Notice("Redmine Engagement started");
+
+            //_database = new PostgresDatabase(_config.Database);
+            //Model.Install(_database, _logger);
+
+            _quaestur = new Quaestur(_config.QuaesturApi);
+            _redmine = new Redmine(_config.RedmineApi);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Sync();
+                System.Threading.Thread.Sleep(60 * 1000);
+            }
+        }
+
+        private void Sync()
+        {
+            _redmine.GetUsers().ToList();
+        }
+    }
+}
