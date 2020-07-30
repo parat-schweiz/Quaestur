9c433218ac1be1ff7393988dcceab02b666d4d71
Stefan Thöni <stefan.thoeni@parat.top>
2020-07-30 10:41:04 +0200

    More data show in billing tab

--- Quaestur/Module/PersonDetailBillingModule.cs
+++ Quaestur/Module/PersonDetailBillingModule.cs
@@ -11,6 +11,9 @@ namespace Quaestur
     {
         public string Id;
         public string Number;
+        public string FromDate;
+        public string UntilDate;
+        public string Amount;
         public string Status;
         public string CreatedDate;
         public string PhraseDeleteConfirmationQuestion;
@@ -19,6 +22,9 @@ namespace Quaestur
         {
             Id = bill.Id.Value.ToString();
             Number = bill.Number.Value.EscapeHtml();
+            FromDate = bill.FromDate.Value.FormatSwissDateDay();
+            UntilDate = bill.UntilDate.Value.FormatSwissDateDay();
+            Amount = bill.Amount.Value.FormatMoney();
             Status = bill.Status.Value.Translate(translator).EscapeHtml();
             CreatedDate = bill.CreatedDate.Value.FormatSwissDateDay();
             PhraseDeleteConfirmationQuestion = translator.Get("Person.Detail.Master.Bills.Delete.Confirm.Question", "Delete bill confirmation question", "Do you really wish to delete bill {0}?", bill.GetText(translator)).EscapeHtml();
@@ -31,6 +37,9 @@ namespace Quaestur
         public string Editable;
         public List<PersonDetailBillItemViewModel> List;
         public string PhraseHeaderNumber;
+        public string PhraseHeaderFromDate;
+        public string PhraseHeaderUntilDate;
+        public string PhraseHeaderAmount;
         public string PhraseHeaderStatus;
         public string PhraseHeaderCreatedDate;
         public string PhraseDeleteConfirmationTitle;
@@ -47,6 +56,9 @@ namespace Quaestur
                 session.HasAccess(person, PartAccess.Billing, AccessRight.Write) ?
                 "editable" : "accessdenied";
             PhraseHeaderNumber = translator.Get("Person.Detail.Bill.Header.Number", "Column 'Number' on the bill tab of the person detail page", "Number").EscapeHtml();
+            PhraseHeaderFromDate = translator.Get("Person.Detail.Bill.Header.FromDate", "Column 'From' on the bill tab of the person detail page", "From").EscapeHtml();
+            PhraseHeaderUntilDate = translator.Get("Person.Detail.Bill.Header.UntilDate", "Column 'Until' on the bill tab of the person detail page", "Until").EscapeHtml();
+            PhraseHeaderAmount = translator.Get("Person.Detail.Bill.Header.Amount", "Column 'Amount' on the bill tab of the person detail page", "Amount").EscapeHtml();
             PhraseHeaderStatus = translator.Get("Person.Detail.Bill.Header.Status", "Column 'Status' on the bill tab of the person detail page", "Status").EscapeHtml();
             PhraseHeaderCreatedDate = translator.Get("Person.Detail.Bill.Header.CreatedDate", "Column 'CreatedDate' on the bill tab of the person detail page", "Created").EscapeHtml();
             PhraseDeleteConfirmationTitle = translator.Get("Person.Detail.Master.Bill.Delete.Confirm.Title", "Delete bill confirmation title", "Delete?").EscapeHtml();
