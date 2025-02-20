4d6f5f8cbb34d1c83f804c27d2fb8d3b06107f01
Stefan Thöni <stefan.thoeni@parat.swiss>
2025-02-20 18:28:25 +0100

    fixup! fixup! fixup! fixup! fixup! Quaestur: Added subscriptions

---
+++ Quaestur/Util/ReadOnlyWidget.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Nancy;
+using Newtonsoft.Json.Linq;
+using SiteLibrary;
+
+namespace Quaestur
+{
+    public class ReadOnlyWidget<TObject> : IWidget<TObject>
+        where TObject : DatabaseObject, new()
+    {
+        public Form Form { get; private set; }
+        public string Id { get; private set; }
+        public int Width { get; private set; }
+        public string PhraseField { get; private set; }
+
+        public string Html => throw new NotImplementedException();
+
+        public string Js => throw new NotImplementedException();
+
+        public string GetValue => throw new NotImplementedException();
+
+        public string SetValidation => throw new NotImplementedException();
+
+        public DatabaseObject UpdatedObject => throw new NotImplementedException();
+
+        public void LoadValue(TObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveValue(PostStatus status, JObject data, TObject obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Quaestur/Util/Widget.cs
+++ Quaestur/Util/Widget.cs
@@ -8,13 +8,78 @@ using SiteLibrary;
 
 namespace Quaestur
 {
-    public abstract class Widget<TObject>
+    public interface IWidget
+    {
+        Form Form { get; }
+        string Id { get; }
+        int Width { get; }
+        string PhraseField { get; }
+
+        string Html { get; }
+        string Js { get; }
+        string GetValue { get; }
+        string SetValidation { get; }
+        DatabaseObject UpdatedObject { get; }
+    }
+
+    public interface IWidget<TObject> : IWidget
+        where TObject : DatabaseObject, new()
+    {
+        void SaveValue(PostStatus status, JObject data, TObject obj);
+        void LoadValue(TObject obj);
+    }
+
+    public abstract class SubWidget<TObject, TSub> : IWidget<TObject>
+        where TObject : DatabaseObject, new()
+        where TSub : DatabaseObject, new()
+    {
+        private readonly IWidget<TSub> _widget;
+        private readonly Func<TObject, bool, TSub> _get;
+
+        public SubWidget(IWidget<TSub> widget, Func<TObject, bool, TSub> get)
+        {
+            _widget = widget;
+            _get = get;
+        }
+
+        public Form Form => _widget.Form;
+        public string Id => _widget.Id;
+        public int Width => _widget.Width;
+        public string PhraseField => _widget.PhraseField;
+
+        public string Html => _widget.Html;
+        public string Js => _widget.Js;
+        public string GetValue => _widget.GetValue;
+        public string SetValidation => _widget.SetValidation;
+        public DatabaseObject UpdatedObject => _widget.UpdatedObject;
+
+        public void LoadValue(TObject obj)
+        {
+            var sub = _get(obj, false);
+            if (sub != null)
+            {
+                _widget.LoadValue(sub);
+            }
+        }
+
+        public void SaveValue(PostStatus status, JObject data, TObject obj)
+        {
+            _widget.SaveValue(status, data, _get(obj, true));
+        }
+    }
+
+    public abstract class Widget<TObject> : IWidget<TObject>
         where TObject : DatabaseObject, new()
     {
         public Form Form { get; private set; }
         public string Id { get; private set; }
         public int Width { get; private set; }
         public string PhraseField { get; private set; }
+
+        public abstract string Html { get; }
+        public abstract string Js { get; }
+        public abstract string GetValue { get; }
+        public abstract string SetValidation { get; }
 
         public Widget(Form form, string id, int width, string phraseField)
         {
@@ -47,12 +112,10 @@ namespace Quaestur
             }
         }
 
-        public abstract string Html { get; }
-        public abstract string Js { get; }
-        public abstract string GetValue { get; }
-        public abstract string SetValidation { get; }
+        public DatabaseObject UpdatedObject { get; protected set; }
 
-        public abstract void AssignValue(PostStatus status, JObject data, TObject obj);
+        public abstract void SaveValue(PostStatus status, JObject data, TObject obj);
+        public abstract void LoadValue(TObject obj);
     }
 
     public abstract class Widget<TObject, TField, TValue> : Widget<TObject>
@@ -68,19 +131,23 @@ namespace Quaestur
 
         public virtual string StringValue
         {
-            get { return Value.ToString(); }
+            get { return Value?.ToString() ?? string.Empty; }
         }
 
         public override string Js { get { return string.Empty; } }
 
         public override string GetValue
         {
-            get { return string.Format("$(\"#{0}{1}\").val();", Form.Id, Id); }
+            get { return string.Format("formData.{1} = $(\"#{0}{1}\").val();", Form.Id, Id); }
         }
 
         public override string SetValidation
         {
             get { return string.Format("assignFieldValidation(\"{0}\", result);", Id); }
+        }
+
+        public override void LoadValue(TObject obj)
+        {
         }
     }
 }
