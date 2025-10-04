30e47927b31a734475be6a693405fc7dea4fccf5
Stefan Thöni <stefan.thoeni@parat.swiss>
2025-10-04 11:44:59 +0200

    Packages: Build debian packages

---
+++ install/debian/trixie/quaestur/create.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+WORKDIR="/tmp/custom-packages/$(uuid)"
+PACKDIR="$WORKDIR/package"
+PROGDIR="$PACKDIR/opt/quaestur"
+CTRLDIR="$PACKDIR/DEBIAN"
+
+mkdir -p "$PROGDIR"
+mkdir -p "$CTRLDIR"
+
+cp -a control "$CTRLDIR/"
+
+cp -a ../../../../BaseLibrary/bin/Release/*.dll "$PROGDIR/"
+cp -a ../../../../SiteLibrary/bin/Release/*.dll "$PROGDIR/"
+cp -a ../../../../SecurityServiceClient/bin/Release/*.dll "$PROGDIR/"
+cp -a ../../../../Quaestur/bin/Release/*.dll "$PROGDIR/"
+cp -a ../../../../Quaestur/bin/Release/*.exe "$PROGDIR/"
+cp -ar ../../../../Quaestur/bin/Release/View "$PROGDIR/"
+cp -ar ../../../../Quaestur/bin/Release/Assets "$PROGDIR/"
+
+DATETIME="$(find "$PACKDIR" -type f -printf '%TY%Tm%Td%TH%TM\n' | sort | tail -n 1)"
+
+chown -R user:user "$PACKDIR"
+find "$PACKDIR" -type d | xargs -I '{}' chmod 0755 {}
+find "$PACKDIR" -type f | xargs -I '{}' chmod 0644 {}
+
+PACKAGE_CONTENT_HASH="$(tar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 1970-01-01' -C $WORKDIR -c package | openssl sha256 -binary | hex)"
+PACKAGE_VERSION="$(package-version version $PACKAGE_CONTENT_HASH)"
+sed -i "s/{{VERSION}}/$PACKAGE_VERSION/g" "$CTRLDIR/control"
+
+PACKAGE_NAME="$(cat control | head -n 1 | cut -d ' ' -f 2)"
+PACKAGE_FULLNAME="${PACKAGE_NAME}_${PACKAGE_VERSION}_amd64"
+find "$PACKDIR" -exec touch -h -t $DATETIME {} \;
+touch -t $DATETIME "$PACKDIR"
+pushd "$WORKDIR" > /dev/null
+dpkg-deb --build --root-owner-group package
+ar -x package.deb 2> /dev/null
+rm package.deb
+ar -qD package.deb debian-binary control.tar.xz data.tar.xz 2> /dev/null
+popd > /dev/null
+cp "$WORKDIR/package.deb" "$1/$PACKAGE_FULLNAME.deb"
+touch -t $DATETIME "$1/$PACKAGE_FULLNAME.deb"
+rm -rf "$WORKDIR"
+
